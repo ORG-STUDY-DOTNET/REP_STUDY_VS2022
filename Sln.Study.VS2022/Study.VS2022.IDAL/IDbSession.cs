4a8ec4599ca0b18e022b1833b7c77a33670a5138
zhangjingrui <zhangjingrui@travelsky.com.cn>
2022-06-13 15:51:57 +0800

    添加 IDbSession

---
+++ Sln.Study.VS2022/Study.VS2022.IDAL/IDbSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Study.VS2022.Model;
+using Study.VS2022.Model.MSSQLServer;
+using Study.VS2022.Model.Oracle;
+
+// 看情况添加
+using Study.VS2022.Model.AutoModels;
+// 第7步:添加 Dals.tt,添加时,要用文本文件为基础
+
+namespace Study.VS2022.IDAL
+{
+	public partial interface IDbSession
+	{
+		ITOrderDal TOrderDal { get; set; }
+
+		ITUserDal TUserDal { get; set; }
+		int SaveChanges();
+	}
+}
