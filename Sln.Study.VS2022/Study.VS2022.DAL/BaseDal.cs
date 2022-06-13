9a3da807680e14d7f70a6cecc0a889fa5c8e14f7
zhangjingrui <zhangjingrui@travelsky.com.cn>
2022-06-13 15:20:22 +0800

    添加 BaseDal

---
+++ Sln.Study.VS2022/Study.VS2022.DAL/BaseDal.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.VS2022.DAL
+{
+    public class BaseDal<T>
+      where T : class, new()
+    {
+        private DbContext db
+        {
+            get
+            {
+                return EFDbContextFactory.GetCurrentDbContext();
+            }
+        }
+
+        public virtual T Add(T entity)
+        {
+            db.Set<T>().Add(entity);
+            return entity;
+        }
+    }
+}
