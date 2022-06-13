3b267ce8dd39e2d541a3e33bff5169cedebb62d4
zhangjingrui <zhangjingrui@travelsky.com.cn>
2022-06-13 14:55:38 +0800

    添加 EFDbContextFactory

---
+++ Sln.Study.VS2022/Study.VS2022.Common/DotNetCoreCallContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.VS2022.Common
+{
+    /// <summary>
+    /// .net core 不再支持 CallContext，需要另实现
+    /// </summary>
+    public static class DotNetCoreCallContext
+    {
+        static ConcurrentDictionary<string, AsyncLocal<object>> state = new ConcurrentDictionary<string, AsyncLocal<object>>();
+
+        public static void SetData(string name, object data)
+        {
+            state.GetOrAdd(name, _ => new AsyncLocal<object>()).Value = data;
+        }
+
+        public static object GetData(string name)
+        {
+            AsyncLocal<object> data;
+            return state.TryGetValue(name, out data) ? data.Value : null;
+        }
+    }
+}
---
+++ Sln.Study.VS2022/Study.VS2022.DAL/EFDbContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Study.VS2022.Common;
+using Study.VS2022.Model.AutoModels;
+using Study.VS2022.Model.MSSQLServer.EFCorePowerTools;
+using Study.VS2022.Model.Oracle.CaseDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.VS2022.DAL
+{
+    public class EFDbContextFactory
+    {
+        // 注意在 Nuget 中统一所依赖库的版本
+        // ------------------------------------
+        public static DbContext GetCurrentDbContext()
+        {
+            DbContext db = (DbContext)DotNetCoreCallContext.GetData("DbContext");
+            if (db == null)
+            {
+                // 需要引用不同的 Model 项目
+                // -------------------------
+                //db = new TestContext();
+                //db = new StudyVS2022Context();
+                db = new CaseContext();
+
+                DotNetCoreCallContext.SetData("DbContext", db);
+            }
+            return db;
+        }
+    }
+}
