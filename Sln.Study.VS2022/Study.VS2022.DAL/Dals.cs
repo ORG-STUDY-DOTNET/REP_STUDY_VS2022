f3308834cd2876e1b79d32effd508301ad2d1fca
zhangjingrui <zhangjingrui@travelsky.com.cn>
2022-06-13 15:37:28 +0800

    添加t4模板，先注释掉类

---
+++ Sln.Study.VS2022/Study.VS2022.DAL/Dals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Study.VS2022.Model;
+using Study.VS2022.Model.MSSQLServer;
+using Study.VS2022.Model.Oracle;
+using Study.VS2022.IDAL;
+// 第7步:添加 Dals.tt,添加时,要用文本文件为基础
+
+namespace Study.VS2022.DAL
+{
+	//public partial class TOrderDal : BaseDal<TOrder>, ITOrderDal
+	//{
+	//}
+
+	//public partial class TUserDal : BaseDal<TUser>, ITUserDal
+	//{
+	//}
+}
---
+++ Sln.Study.VS2022/Study.VS2022.IDAL/IBaseDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.VS2022.IDAL
+{
+    public interface IBaseDal
+    {
+    }
+}
