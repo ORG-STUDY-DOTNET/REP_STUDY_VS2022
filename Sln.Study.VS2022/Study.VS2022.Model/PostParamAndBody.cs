391cc2ad81249bfdbed74c5ebd17ca26174055b5
ruilovechu <ruilovechu@live.com>
2022-06-21 16:10:50 +0800

    一些 get post 请求的接口写法示例

---
+++ Sln.Study.VS2022/Study.VS2022.Model/PostParamAndBody.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.VS2022.Model
+{
+    /// <summary>
+    /// 一个 WebAPI 接口里只能有一个 FormBody。
+    /// </summary>
+    public class PostParamAndBody
+    {
+        /// <summary>
+        /// 页面大小
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 页码
+        /// </summary>
+        public int PageIndex { get; set; }
+
+        /// <summary>
+        /// 文件
+        /// </summary>
+        public IFormFile File { get; set; }
+    }
+}
--- Sln.Study.VS2022/Study.VS2022.WebAPI/Areas/AR1/Controllers/AH1Controller.cs
+++ Sln.Study.VS2022/Study.VS2022.WebAPI/Areas/AR1/Controllers/AH1Controller.cs
@@ -89,8 +89,61 @@ namespace Study.VS2022.WebAPI.Areas.AR1.Controllers
             return jr;
         }
 
+
+
+        #region 测试中的写法
+
+      
+
+        #endregion
+
+        #region 测试通过的写法
+
         /// <summary>
-        /// post 传递固定的多组 文件与 form
+        /// Json 不能和文件同时提交，不要重载， Swagger 会无法显示
+        /// </summary>
+        [DisableRequestSizeLimit]
+        [HttpPost]
+        public JsonResult PostJson([FromQuery] string id, [FromBody] PostParam f1)
+        {
+            #region 客户端写法
+
+            /*
+    var data = {
+        PageSize: "12",
+        PageIndex: "15"
+    };
+
+    //创建XMLHttpRequest异步对象
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", url);
+
+    // 
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    //请求完毕
+    xhr.onload = function () {
+        console.log('in onload');
+    }
+
+    //发送
+    var msg = JSON.stringify(data);
+    xhr.send(msg);
+             */
+
+            #endregion
+
+            JsonResult jr = new JsonResult(new
+            {
+                Ret = 1,
+                Msg = "OK"
+            });
+            return jr;
+        }
+
+        /// <summary>
+        ///  post 传递固定的多组 文件与 form。 独立的 IFormFile 参数不可以加 FromForm 特性
         /// </summary>
         /// <param name="id"></param>
         /// <param name="file1"></param>
@@ -100,8 +153,199 @@ namespace Study.VS2022.WebAPI.Areas.AR1.Controllers
         /// <returns></returns>
         [DisableRequestSizeLimit]
         [HttpPost]
-        public JsonResult PostFileAndForm([FromQuery] string id, IFormFile file1, [FromForm]string f1
+        public JsonResult PostFileAndForm([FromQuery] string id, IFormFile file1, [FromForm] string f1
             , IFormFile file2, [FromForm] string f2)
+        {
+            #region 客户端写法
+            /*
+    //创建XMLHttpRequest异步对象
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", url);
+
+    // 
+    //xhr.setRequestHeader("Content-Type", "multipart/form-data");
+
+    //监视上传进度
+    if (xhr.upload) {
+        xhr.upload.onprogress = function (e) {
+            if (e.lengthComputable) {
+                console.log(e.loaded, e.total);
+            }
+        };
+    }
+
+    //请求完毕
+    xhr.onload = function () {
+        console.log('in onload');
+    }
+
+    //发送
+    var data = new FormData();
+    var file1 = document.getElementById("id_f1").files[0];
+    data.append("file1", file1);
+    data.append("f1", 'dea');
+    data.append("file2", file1);
+    data.append("f2", '3ab');
+    xhr.send(data);
+             */
+            #endregion
+
+            JsonResult jr = new JsonResult(new
+            {
+                Ret = 1,
+                Msg = "OK"
+            });
+            return jr;
+        }
+
+        /// <summary>
+        /// Post 提交，参数实体里带文件的方式，这个实体类型一定为 FromForm ，如果为 FromBody ，则接口端拿不到文件
+        /// 前端调用未验证
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="file1"></param>
+        /// <param name="f1"></param>
+        /// <param name="file2"></param>
+        /// <param name="f2"></param>
+        /// <returns></returns>
+        [DisableRequestSizeLimit]
+        [HttpPost]
+        public JsonResult PostFileAndFormBody([FromQuery] string id, [FromForm] PostParamAndBody body)
+        {
+            #region 客户端写法
+
+            /*
+    //创建XMLHttpRequest异步对象
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", url);
+
+    // 这里带了文件，自动转为了 json，即使后端使用的是 FromForm
+    // 写完这一句，拿不到文件了，是因为有 OPTIONS 请求？
+    //xhr.setRequestHeader("Content-Type", "application/json");
+
+    //监视上传进度
+    if (xhr.upload) {
+        xhr.upload.onprogress = function (e) {
+            if (e.lengthComputable) {
+                console.log(e.loaded, e.total);
+            }
+        };
+    }
+
+    //请求完毕
+    xhr.onload = function () {
+        console.log('in onload');
+    }
+
+    //发送
+    var data = new FormData();
+    var file1 = document.getElementById("id_f1").files[0];
+    data.append("PageSize", '11');
+    data.append("PageIndex", '12');
+    data.append("File", file1);
+    xhr.send(data);             
+             */
+
+            #endregion
+
+            JsonResult jr = new JsonResult(new
+            {
+                Ret = 1,
+                Msg = "OK"
+            });
+            return jr;
+        }
+
+        /// <summary>
+        /// 不定项的字符串与文件
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="s1"></param>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        [DisableRequestSizeLimit]
+        [HttpPost]
+        public JsonResult PostMulFiles([FromQuery] string id, [FromForm] List<string> s1, IList<IFormFile> files)
+        {
+            #region 客户端写法
+            /*
+    //创建XMLHttpRequest异步对象
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", url);
+
+    // 这里带了文件，自动转为了 json，即使后端使用的是 FromForm
+    // 写完这一句，拿不到文件了，是因为有 OPTIONS 请求？
+    //xhr.setRequestHeader("Content-Type", "application/json");
+
+    //监视上传进度
+    if (xhr.upload) {
+        xhr.upload.onprogress = function (e) {
+            if (e.lengthComputable) {
+                console.log(e.loaded, e.total);
+            }
+        };
+    }
+
+    //请求完毕
+    xhr.onload = function () {
+        console.log('in onload');
+    }
+
+    //发送
+    var data = new FormData();
+    var file1 = document.getElementById("id_f1").files[0];
+    data.append("s1", '11');
+    data.append("s1", '12');
+    data.append("files", file1);
+    data.append("files", file1);
+    xhr.send(data);
+             */
+            #endregion
+
+            JsonResult jr = new JsonResult(new
+            {
+                Ret = 1,
+                Msg = "OK"
+            });
+            return jr;
+        }
+
+        #endregion
+
+        #region 不可行的写法
+
+        /// <summary>
+        /// 这种情况不可行，body 中拿不到任何内容
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        [Obsolete]
+        [DisableRequestSizeLimit]
+        [HttpPost]
+        public JsonResult PostFileAndFormForms([FromQuery] string id, [FromForm] List<PostParamAndBody> body)
+        {
+            JsonResult jr = new JsonResult(new
+            {
+                Ret = 1,
+                Msg = "OK"
+            });
+            return jr;
+        }
+
+        /// <summary>
+        /// 这种情况不可行，在 Swagger 中没有办法传入文件，且直接返回 400
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        [Obsolete]
+        [DisableRequestSizeLimit]
+        [HttpPost]
+        public JsonResult PostFileAndFormBodys([FromQuery] string id, [FromBody] List<PostParamAndBody> body)
         {
             JsonResult jr = new JsonResult(new
             {
@@ -118,9 +362,10 @@ namespace Study.VS2022.WebAPI.Areas.AR1.Controllers
         /// <param name="file1"></param>
         /// <param name="f1"></param>
         /// <returns></returns>
+        [Obsolete]
         [DisableRequestSizeLimit]
         [HttpPost]
-        public JsonResult PostJsonError([FromQuery] string id, IFormFile file1, [FromBody]PostParam f1)
+        public JsonResult PostJsonError([FromQuery] string id, IFormFile file1, [FromBody] PostParam f1)
         {
             JsonResult jr = new JsonResult(new
             {
@@ -130,20 +375,29 @@ namespace Study.VS2022.WebAPI.Areas.AR1.Controllers
             return jr;
         }
 
-        /// <summary>
-        /// Json 不能和文件同时提交，不要重载， Swagger 会无法显示
-        /// </summary>
-        [DisableRequestSizeLimit]
-        [HttpPost]
-        public JsonResult PostJson([FromQuery] string id, [FromBody] PostParam f1)
-        {
-            JsonResult jr = new JsonResult(new
-            {
-                Ret = 1,
-                Msg = "OK"
-            });
-            return jr;
-        }
+        ///// <summary>
+        ///// 多组 FromForm 的情况：会导致 Swagger 报错
+        ///// </summary>
+        ///// <param name="id"></param>
+        ///// <param name="body1"></param>
+        ///// <param name="body2"></param>
+        ///// <returns></returns>
+        //[DisableRequestSizeLimit]
+        //[HttpPost]
+        //public JsonResult PostFileAndMulFormBody([FromQuery] string id, [FromForm]PostParamAndBody body1
+        //    , [FromForm] PostParamAndBody body2)
+        //{
+        //    JsonResult jr = new JsonResult(new
+        //    {
+        //        Ret = 1,
+        //        Msg = "OK"
+        //    });
+        //    return jr;
+        //}
+
+        #endregion
+
+      
 
 
     }
