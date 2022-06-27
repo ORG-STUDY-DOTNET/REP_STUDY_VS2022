6a152c9c6e3677c095de7f138c08a6dcb9f0dc4c
ruilovechu <ruilovechu@live.com>
2022-06-27 10:32:54 +0800

    不带 Header 身份信息的请求

---
+++ Sln.Study.VS2022/Study.VS2022.ConsoleAPP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace Study.VS2022.ConsoleAPP
+{
+    internal class Program
+    {
+        static void GetTest()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5014/");
+            //client.Timeout = new TimeSpan(0);
+            Task<string> tsres = client.GetStringAsync("/api/AR1/AH1/GetDemoAuth");
+            tsres.Wait();
+
+            string res = tsres.Result;
+        }
+
+        static void Main(string[] args)
+        {
+            // Get 请求测试
+            // ------------
+            GetTest();
+
+            Console.ReadKey();
+        }
+    }
+}
