f00cdba4a67b2b3aca4ea97ba2167337a0fb32d1
ruilovechu <ruilovechu@live.com>
2022-07-21 13:26:26 +0800

    权限不满足某一种 Filter 时，直接返回 401

--- Sln.Study.VS2022/Study.VS2022.WebAPI/Filters/TestFilterAttribute.cs
+++ Sln.Study.VS2022/Study.VS2022.WebAPI/Filters/TestFilterAttribute.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
 
 namespace Study.VS2022.WebAPI.Filters
 {
@@ -20,7 +22,19 @@ context.HttpContext.Request.Path
             // 用户名：
             string currentUserName = context.HttpContext.User.Identity.Name;
 
-
+            // 如果有用户名
+            if (!String.IsNullOrWhiteSpace(currentUserName))
+            {
+                // 如果不是 System，则返回 401 无权限
+                if (!"System".Equals(currentUserName))
+                {
+                    context.Result = new ContentResult
+                    {
+                        StatusCode = (int)HttpStatusCode.Unauthorized
+                    };
+                    return;
+                }
+            }
 
             base.OnActionExecuting(context);
         }
