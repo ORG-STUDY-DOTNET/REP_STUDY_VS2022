31d67397a0c7990b17e765da649ba2c2849a8096
ruilovechu <ruilovechu@live.com>
2022-07-25 12:14:24 +0800

    打桩注册接口

---
+++ Sln.Study.VS2022/Study.VS2022.WebAPI/Areas/ARUser/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Study.VS2022.WebAPI.Areas.AR1.Models;
+using Study.VS2022.WebAPI.Areas.ARUser.Models;
+using Study.VS2022.WebAPI.Filters;
+
+namespace Study.VS2022.WebAPI.Areas.ARUser.Controllers
+{
+    /// <summary>
+    /// 用户相关
+    /// </summary>
+    [TestFilter]
+    [DisableRequestSizeLimit]
+    [Authorize]
+    [Area("ARUser")] // 添加 Area 和 Route 两个特性
+    [Route("api/[area]/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// 注册接口
+        /// </summary>
+        [AllowAnonymous]
+        [HttpPost]
+        public JsonResult Register([FromForm]RegisterBody regBody)
+        {
+            JsonResult jr = new JsonResult(new RetData(ERet.TECH_ERROR, "未实现", null));
+            return jr;
+        }
+    }
+}
---
+++ Sln.Study.VS2022/Study.VS2022.WebAPI/Areas/ARUser/Models/RegisterBody.cs
@@ -0,0 +1,33 @@
+﻿namespace Study.VS2022.WebAPI.Areas.ARUser.Models
+{
+    /// <summary>
+    /// 提交注册的内容
+    /// </summary>
+    public class RegisterBody
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 加密的密码
+        /// </summary>
+        public string UserPassword { get; set; }
+
+        /// <summary>
+        /// 加密的密码（对重复密码进行加密）
+        /// </summary>
+        public string UserPasswordConfirm { get; set; }
+
+        /// <summary>
+        /// 验证码编号
+        /// </summary>
+        public string ValidateGuid { get; set; }
+
+        /// <summary>
+        /// 验证码
+        /// </summary>
+        public string ValidateCode { get; set; }
+    }
+}
